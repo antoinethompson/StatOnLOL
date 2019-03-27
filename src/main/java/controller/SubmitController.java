7bd2d1919529a03b7e59ffcc55ececf4be9e0105
Antoine <toine.thompson@gmail.com>
2019-03-27 20:45:46 +0100

    ajout de back

---
+++ src/main/java/controller/SubmitController.java
@@ -0,0 +1,14 @@
+package controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+
+@Controller
+public class SubmitController {
+
+    @PostMapping("/submit")
+    public String redirectionProfile(){
+        return "profile";
+    }
+}
